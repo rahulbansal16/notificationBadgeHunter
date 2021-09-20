4b1043bfe397561dc42bfb421127a83183e92f03
rabans <bansal.rahul14@gmail.com>
2021-09-20 17:09:50 +0530

    commented code for updating the favicon

--- js/detect.js
+++ js/detect.js
@@ -5,14 +5,20 @@ const getHost = () => {
 }
 // #c42523
 // removeBadges(getHost())
+// const links = document.querySelectorAll('link[rel=icon]')
+// console.log('The links are', links)
+// href="https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png"
 chrome.runtime.onMessage.addListener((request) => {
     console.log('In the Listener for the block-ad ')
     const {type} = request
     if (type === "block-ad"){
         modifyTitle(getHost())
+        // const updatedLinks = document.querySelectorAll('link[rel=icon]')
+        // for (var i in updatedLinks){
+        //     updatedLinks[i].href = links[i].href
+        // }
     }
 })
-
 console.log(getHost())
 var style = document.documentElement.appendChild(document.createElement('style'));
 style.textContent = generateCSSRule(getHost());
