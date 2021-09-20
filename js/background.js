7f3d8726b459b1bb1abcfa0570877b386736deff
rabans <bansal.rahul14@gmail.com>
2021-09-20 11:34:48 +0530

    removed the unwanted events

--- js/background.js
+++ js/background.js
@@ -1,37 +1,44 @@
-const raiseBlockAdEvent = (tabid) => {
-    console.log('Raised a Block Ad Event')
-    chrome.tabs.sendMessage(tabid, {
-        type: 'block-ad'
-    })
-}
+// const raiseBlockAdEvent = (tabid) => {
+//     console.log('Raised a Block Ad Event')
+//     chrome.tabs.sendMessage(tabid, {
+//         type: 'block-ad'
+//     })
+// }
 
-const fetchTabs = () => {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({active: true}, resolve)
-    })
-}
+// const raiseMutationObserverEvent = (tabid) =>{
+//     console.log('Raised a Block Ad Event')
+//     chrome.tabs.sendMessage(tabid, {
+//         type: 'block-ad'
+//     })
+// }
 
-chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
-    console.log('Inside the onHistoryStateUpdated method')
-    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-    // chrome.tabs.query({active: true, currentWindow: true},
-    //     function(tabs){
-    //         if(tabs[0].id) raiseBlockAdEvent(tabs[0].id)
-    //     }
-    // );
-    // chrome.tabs.sendMessage({
-    //     type:'block-ad'
-    // })
-    // fireBlockAd()
-})
+// const fetchTabs = () => {
+//     return new Promise((resolve, reject) => {
+//         chrome.tabs.query({active: true}, resolve)
+//     })
+// }
 
-chrome.webNavigation.onCompleted.addListener(() => {
-    console.log('Inside the onCompleted method')
-    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-})
+// chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
+//     console.log('Inside the onHistoryStateUpdated method')
+//     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
+//     // chrome.tabs.query({active: true, currentWindow: true},
+//     //     function(tabs){
+//     //         if(tabs[0].id) raiseBlockAdEvent(tabs[0].id)
+//     //     }
+//     // );
+//     // chrome.tabs.sendMessage({
+//     //     type:'block-ad'
+//     // })
+//     // fireBlockAd()
+// })
 
-// });
-// chrome.webNavigation.onCommitted.addListener(() => {
-//     console.log('Inside the onCommitted method')
+// chrome.webNavigation.onCompleted.addListener(() => {
+//     console.log('Inside the onCompleted method')
 //     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
 // })
+
+// // });
+// // chrome.webNavigation.onCommitted.addListener(() => {
+// //     console.log('Inside the onCommitted method')
+// //     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
+// // })
