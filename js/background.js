91449c596d3735a1a79899f7b6beb6dc779009b8
rabans <bansal.rahul14@gmail.com>
2021-09-20 07:39:48 +0530

    added events to block notification on page loaded

--- js/background.js
+++ js/background.js
@@ -1,12 +1,37 @@
+const raiseBlockAdEvent = (tabid) => {
+    console.log('Raised a Block Ad Event')
+    chrome.tabs.sendMessage(tabid, {
+        type: 'block-ad'
+    })
+}
+
+const fetchTabs = () => {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({active: true}, resolve)
+    })
+}
+
 chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
     console.log('Inside the onHistoryStateUpdated method')
-    chrome.tabs.query({active: true, currentWindow: true},
-        function(tabs){
-            if(tabs[0].id) chrome.tabs.sendMessage(tabs[0].id, {type: "block-ad"});
-        }
-    );
+    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
+    // chrome.tabs.query({active: true, currentWindow: true},
+    //     function(tabs){
+    //         if(tabs[0].id) raiseBlockAdEvent(tabs[0].id)
+    //     }
+    // );
     // chrome.tabs.sendMessage({
     //     type:'block-ad'
     // })
     // fireBlockAd()
+})
+
+chrome.webNavigation.onCompleted.addListener(() => {
+    console.log('Inside the onCompleted method')
+    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
+})
+
+// });
+chrome.webNavigation.onCommitted.addListener(() => {
+    console.log('Inside the onCommitted method')
+    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
 })
