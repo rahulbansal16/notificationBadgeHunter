3c4085faa295d81af3d2cda83889cb66c3783578
rabans <bansal.rahul14@gmail.com>
2021-09-25 09:31:58 +0530

    Fixed the formatting

--- js/background.js
+++ js/background.js
@@ -1,22 +1,22 @@
 const raiseBlockAdEvent = (tabid) => {
-    console.log('Raised a Block Ad Event')
-    chrome.tabs.sendMessage(tabid, {
-        type: 'block-ad'
-    })
-}
+  console.log("Raised a Block Ad Event");
+  chrome.tabs.sendMessage(tabid, {
+    type: "block-ad",
+  });
+};
 
 const fetchTabs = () => {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.query({active: true}, resolve)
-    })
-}
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true }, resolve);
+  });
+};
 
-chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
-    console.log('Inside the onHistoryStateUpdated method')
-    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-})
+chrome.webNavigation.onHistoryStateUpdated.addListener(() => {
+  console.log("Inside the onHistoryStateUpdated method");
+  fetchTabs().then((tabs) => tabs[0].id && raiseBlockAdEvent(tabs[0].id));
+});
 
 chrome.webNavigation.onCompleted.addListener(() => {
-    console.log('Inside the onCompleted method')
-    fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-})
+  console.log("Inside the onCompleted method");
+  fetchTabs().then((tabs) => tabs[0].id && raiseBlockAdEvent(tabs[0].id));
+});
--- js/detect.js
+++ js/detect.js
@@ -1,37 +1,39 @@
 const getHost = () => {
-    let host_ = window.location.host
-    host_ = host_.replace("www.","")
-    return host_
-}
-const host = getHost()
+  let host_ = window.location.host;
+  host_ = host_.replace("www.", "");
+  return host_;
+};
+const host = getHost();
 // #c42523
 // removeBadges(getHost())
 // const links = document.querySelectorAll('link[rel=icon]')
 // console.log('The links are', links)
 // href="https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png"
 chrome.runtime.onMessage.addListener((request) => {
-    console.log('In the Listener for the block-ad ')
-    const {type} = request
-    if (type === "block-ad"){
-        modifyTitle(host)
-        // const updatedLinks = document.querySelectorAll('link[rel=icon]')
-        // for (var i in updatedLinks){
-        //     updatedLinks[i].href = links[i].href
-        // }
-    }
-})
-console.log(host)
-var style = document.documentElement.appendChild(document.createElement('style'));
+  console.log("In the Listener for the block-ad ");
+  const { type } = request;
+  if (type === "block-ad") {
+    modifyTitle(host);
+    // const updatedLinks = document.querySelectorAll('link[rel=icon]')
+    // for (var i in updatedLinks){
+    //     updatedLinks[i].href = links[i].href
+    // }
+  }
+});
+console.log(host);
+var style = document.documentElement.appendChild(
+  document.createElement("style")
+);
 style.textContent = generateCSSRule(host);
 // style.textContent = '.indicator-badge {display:none !important;}';
 
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('./js/mosf.js');
-s.onload = function() {
-    this.remove();
+var s = document.createElement("script");
+s.src = chrome.extension.getURL("./js/mosf.js");
+s.onload = function () {
+  this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
 
 chrome.runtime.sendMessage({
-    type:'block-http'
-})
+  type: "block-http",
+});
