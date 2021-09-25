1896abdacc05867e00291c55f51e21025208acfe
rabans <bansal.rahul14@gmail.com>
2021-09-25 09:11:50 +0530

    removed unnecessary code from background

--- js/background.js
+++ js/background.js
@@ -5,13 +5,6 @@ const raiseBlockAdEvent = (tabid) => {
     })
 }
 
-// const raiseMutationObserverEvent = (tabid) =>{
-//     console.log('Raised a Block Ad Event')
-//     chrome.tabs.sendMessage(tabid, {
-//         type: 'block-ad'
-//     })
-// }
-
 const fetchTabs = () => {
     return new Promise((resolve, reject) => {
         chrome.tabs.query({active: true}, resolve)
@@ -21,24 +14,9 @@ const fetchTabs = () => {
 chrome.webNavigation.onHistoryStateUpdated.addListener( () => {
     console.log('Inside the onHistoryStateUpdated method')
     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-    // chrome.tabs.query({active: true, currentWindow: true},
-    //     function(tabs){
-    //         if(tabs[0].id) raiseBlockAdEvent(tabs[0].id)
-    //     }
-    // );
-    // chrome.tabs.sendMessage({
-    //     type:'block-ad'
-    // })
-    // fireBlockAd()
 })
 
 chrome.webNavigation.onCompleted.addListener(() => {
     console.log('Inside the onCompleted method')
     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
 })
-
-// // });
-// // chrome.webNavigation.onCommitted.addListener(() => {
-// //     console.log('Inside the onCommitted method')
-// //     fetchTabs().then( tabs => tabs[0].id && raiseBlockAdEvent(tabs[0].id))
-// // })
