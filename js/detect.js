15fe0471ac5867793e048419c3f8c0d4e2c3f142
rabans <bansal.rahul14@gmail.com>
2021-09-25 11:01:38 +0530

    added the notification toggle

--- js/detect.js
+++ js/detect.js
@@ -1,39 +1,53 @@
+chrome.storage.sync.get(['notification'], (response) => {
+    console.log('In the detect.js reading the notification value', response);
+    const notification = response.notification
+    if ( notification === "off"){
+        initExtension()
+    } else {
+        return
+    }
+})
+
 const getHost = () => {
-  let host_ = window.location.host;
-  host_ = host_.replace("www.", "");
-  return host_;
+    let host_ = window.location.host;
+    host_ = host_.replace("www.", "");
+    return host_;
 };
-const host = getHost();
-// #c42523
-// removeBadges(getHost())
-// const links = document.querySelectorAll('link[rel=icon]')
-// console.log('The links are', links)
-// href="https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png"
-chrome.runtime.onMessage.addListener((request) => {
-  console.log("In the Listener for the block-ad ");
-  const { type } = request;
-  if (type === "block-ad") {
-    modifyTitle(host);
-    // const updatedLinks = document.querySelectorAll('link[rel=icon]')
-    // for (var i in updatedLinks){
-    //     updatedLinks[i].href = links[i].href
-    // }
-  }
-});
-console.log(host);
-var style = document.documentElement.appendChild(
-  document.createElement("style")
-);
-style.textContent = generateCSSRule(host);
-// style.textContent = '.indicator-badge {display:none !important;}';
 
-var s = document.createElement("script");
-s.src = chrome.extension.getURL("./js/mosf.js");
-s.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+const initExtension = () => {
+    const host = getHost();
+    // #c42523
+    // removeBadges(getHost())
+    // const links = document.querySelectorAll('link[rel=icon]')
+    // console.log('The links are', links)
+    // href="https://www.redditstatic.com/desktop2x/img/favicon/favicon-32x32.png"
+    chrome.runtime.onMessage.addListener((request) => {
+        console.log("In the Listener for the block-ad ");
+        const { type } = request;
+        if (type === "block-ad") {
+        modifyTitle(host);
+        // const updatedLinks = document.querySelectorAll('link[rel=icon]')
+        // for (var i in updatedLinks){
+        //     updatedLinks[i].href = links[i].href
+        // }
+        }
+    });
+    console.log(host);
+    var style = document.documentElement.appendChild(
+        document.createElement("style")
+    );
+    style.textContent = generateCSSRule(host);
+    // style.textContent = '.indicator-badge {display:none !important;}';
 
-chrome.runtime.sendMessage({
-  type: "block-http",
-});
+    var s = document.createElement("script");
+    s.src = chrome.extension.getURL("./js/mosf.js");
+    s.onload = function () {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
+
+    chrome.runtime.sendMessage({
+        type: "block-http",
+    });
+}
+
